18b6af20c62622a7d9453de67e6a2dcb86302b70
Ben Bates <bates.benjamin@gmail.com>
2012-06-19 21:58:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HubwayDir::Application.config.secret_token = '37b5e489bb9db98287c342ba8bc9f9e04ca53c2febadfbcdbdc96d526d6cbab331b6dce7ccf4af40a1865ce7ef45bdc5301e5fa88a03eb7dac8b66896aab2cc9'
